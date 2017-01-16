6e6f68c96a17f7494bddcb5dc5f6b6236a1713b9
choco <choco@rambler-co.ru>
2017-01-16 21:49:34 +0300

    new changes

--- projects/cgp_update_cluster/cgp_up_cluster.py
+++ projects/cgp_update_cluster/cgp_up_cluster.py
@@ -1,14 +1,44 @@
 #!/usr/bin/env python
 
+import socket
+import requests
 
 def main():
+    port=8274
     #filename = 'hosts.mail'
     filename = 'hosts.rambler'
-    outfromfile = fileRead(filename)
-    if outfromfile :
-        print outfromfile
+    domains = fileRead(filename)
+    if domains :
+        agrigated = resolvDomain(port,domains)
+        for port_status,domain,ipaddr in agrigated:
+            if not port_status:
+                print domain, ipaddr, 'close'
+            else:
+                print domain, ipaddr, 'open'
+                
 
-def cutdomain(dom):
+def resolvDomain(port,*args):
+    for domain in args[0]:
+        try:
+            ip = socket.gethostbyname(domain)
+        except socket.error, msg:
+             print domain,msg
+             ip = None
+             yield False, domain, ip
+        if ip : 
+            try:
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.settimeout(5)
+                s.connect((domain, port))
+                s.close()
+                yield True, domain, ip
+            except socket.error:
+                yield False, domain, ip
+
+def toFqdn(num):
+    return 'mail{0}.rambler.ru'.format(num)
+
+def cutDomain(dom):
     if dom and dom.find('.') != -1:
        return dom[:dom.index('.')]
     else:
@@ -17,14 +47,10 @@ def cutdomain(dom):
 def fileRead(file):
     try:
         with open(file, "r") as fhandler :
-            #listofnums = []
-            listofnums = [ cutdomain(line.strip('mail').strip()) for line in fhandler ]
-    #        for line in fhandler:
-    #            num = cutdomain(line.strip('mail').strip())
-    #            if num :
-    #               listofnums.append(num) 
-                
-        return sorted(listofnums, key=int)
+            listofnums = [ cutDomain(line).strip().strip('mail') for line in fhandler ]
+            sortednums = sorted(listofnums, key=int)
+            return [ toFqdn(num) for num in sortednums ]
+
     except IOError as err :
             print 'File Error:', str(err)
             return None
